c67b917c3b2064673e39310d562a01d790c96f24
Sebastian Jentschke <Sebastian.Jentschke@gmail.com>
2020-07-14 13:07:59 +0200

    First working version - User manual

--- conf.py
+++ conf.py
@@ -1,24 +1,20 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
+# This file only contains a selection of the most common options.
+# For a full list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Path setup --------------------------------------------------------------
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
-
 project = u'jamovi'
-copyright = u'2020-, Sebastian Jentschke for `The jamovi Group <http://www.jamovi.org/>`_'
+copyright = u'2020-, Sebastian Jentschke for The jamovi Group'
 author = u'Sebastian Jentschke'
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
